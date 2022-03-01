761bdf37b522b631c2d4bb96765301e7849bdba0
Felix Brinadze <felix.brinadze@gmail.com>
2022-02-28 21:14:47 -0500

    first commit from the command line

---
+++ sql_class_4.sql
@@ -0,0 +1,8 @@
+/*Count example*/
+use sakila;
+SELECT *
+FROM actor;
+
+use human_resources;
+SELECT count(middleName)
+FROM employees;
--- thursday.sql
+++ thursday.sql
@@ -26,32 +26,10 @@ LIMIT 10;
 -- IN
 -- Select the title and rating of films that have a rental rate of less than $4 and are rated R or NC-17.
  -- Select the title and rating of films that begin with T and have a duration of 3, 5, or 7 days. Note that since duration is numerical, you do not need quotes.
-/*in example*/
-SELECT title, rating
-FROM film
-WHERE rental_rate < 4.00 and rating in ('R', 'NC-17');
-
-SELECT title, rating 
-FROM film
-WHERE title like 'T%' and rental_duration in (3,5,7);
+ 
 -- BETWEEN
 -- Select the title and duration of films that have a duration of between 90 and 120 minutes.
 -- Select the film_id and title of films that have a film_id of between 100 and 201.
- /*between example*/
- SELECT title, length
- FROM film
- WHERE length between 90 and 120;
  
-SELECT title, length
-FROM film
-WHERE film_id between 100 and 201;
-
-
 -- Aliases
--- Select the title, rental_duration, and rental_rate of all films that begin with the letter C. Rename rental_duration to Duration and rental_rate to Rental Rate.
-/* Aliases*/
-SELECT title, 
-	   rental_duration as 'Duration', 
-       rental_rate as 'Rental_Rate'
-FROM film
-WHERE title like 'C%'
+-
