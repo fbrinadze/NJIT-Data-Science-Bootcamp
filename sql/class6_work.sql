28902a13ddb1945030a21566c5a983f5e932e824
Felix Brinadze <felix.brinadze@gmail.com>
2022-03-01 20:36:05 -0500

    updating class 6

--- sql/class6_work.sql
+++ sql/class6_work.sql
@@ -40,7 +40,72 @@ SELECT
 FROM
     CUSTOMER_INFO;
 
--- DELETE from CUSTOMER_INFO;
-DESCRIBE customer_info;
+
+-- Update & rollback
+SET autocommit = ON;
+
+update customer_info
+set first_name = 'After'
+WHERE last_name = 'SMITH';
+
+ROLLBACK;
+
+select * from customer_info where last_name = 'smith';
+
+-- TEMP tables
+
+CREATE temporary Table temp_customer_1
+select * from customer
+where address_id = 5;
+
+SELECT * from temp_customer_1;
+
+-- WITH statement
+WITH demo_table AS
+(select first_name, last_name from customer)
+select * from demo_table;
+
+-- Subquery
+SELECT firstname, lastname, title, salary
+from employees
+WHERE salary = (SELECT max(salary) from employees);
+
+SELECT firstname, lastname, title, salary
+from employees
+WHERE salary < (select avg(Salary) from employees)
+ORDER BY salary DESC;
+
+SELECT * from employees
+where employeeid in (select employeeid from addresses where state = 'MA');
+
+SELECT *
+FROM ( 
+	select c.customer_id, c.first_name, c.last_name, p.rental_id, p.amount
+    from payment as p
+    inner join customer c
+    on c.customer_id = p.customer_id
+) as base_table;
+
+-- TESTing for NULLS
+
+SELECT * FROM EMPLOYEES
+where
+middlename is null;
+
+SELECT * FROM EMPLOYEES
+where
+middlename is NOT null;
+
+SELECT firstname, middlename, lastname, ifnull(middlename, '*') as NEW_MIDDLE_NAME
+FROM employees
+order by MiddleName;
+
+
+
+
+
+
+
+
 
 
